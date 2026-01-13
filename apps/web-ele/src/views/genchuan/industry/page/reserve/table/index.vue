319723ebb04e0d74d976f95ca6c13c0fd9ddda14
DaPeGood <944782441@qq.com>
2026-01-13 17:06:28 +0800

    feat: 停车诱导服务、停车预约服务修改

--- apps/web-ele/src/views/genchuan/industry/page/reserve/table/index.vue
+++ apps/web-ele/src/views/genchuan/industry/page/reserve/table/index.vue
@@ -23,10 +23,23 @@ const props = defineProps({
 const getTitle = computed(() => {
   return formData.value?.id ? textObj.editText : textObj.addText;
 });
+
 const [Drawer, drawerApi] = useVbenDrawer({
+  modal: false,
+  appendToMain: true,
   footer: false,
   onCancel() {
     drawerApi.close();
+  },
+  onConfirm() {},
+  async onOpenChange() {},
+});
+const [DetailDrawer, detailDrawerApi] = useVbenDrawer({
+  modal: false,
+  appendToMain: true,
+  footer: false,
+  onCancel() {
+    detailDrawerApi.close();
   },
   onConfirm() {},
   async onOpenChange() {},
@@ -45,6 +58,8 @@ const [Form, formApi] = useVbenForm({
   showDefaultActions: false,
 });
 const [FormDrawer, formDrawerApi] = useVbenDrawer({
+  appendToMain: true,
+  modal: false,
   onCancel() {
     formDrawerApi.close();
   },
@@ -73,7 +88,6 @@ const [FormDrawer, formDrawerApi] = useVbenDrawer({
     }
   },
 });
-const [searchDrawer] = useVbenDrawer();
 /** 刷新表格 */
 function handleRefresh() {
   gridApi.query();
@@ -137,12 +151,17 @@ function handleRowCheckboxChange({ records }) {
   checkedIds.value = records.map((item) => item.id);
 }
 const dataObj = reactive({
+  totalShow: false,
+  detailObj: {},
   total: dataList().length,
   currentPage: 1,
   pageSize: 10,
   apilist: dataList(),
   list: [],
 });
+const changeTotalShow = () => {
+  dataObj.totalShow = !dataObj.totalShow;
+};
 // 表格数据获取
 const getTableData = (pageObj) => {
   const page = pageObj.page;
@@ -186,7 +205,12 @@ const [QueryForm] = useVbenForm({
   // 垂直布局，label和input在不同行，值为vertical
   // 水平布局，label和input在同一行
   layout: 'horizontal',
-  schema: useFormSchema(),
+  schema: useFormSchema().map((v) => {
+    delete v.rules;
+    return {
+      ...v,
+    };
+  }),
   // 是否可展开
   showCollapseButton: true,
   submitButtonOptions: {
@@ -226,7 +250,25 @@ const [Grid, gridApi] = useVbenVxeGrid({
 });
 
 const activeName = ref('全部');
-const tabsData = ref([{ label: '全部' }, { label: '启用' }, { label: '禁用' }]);
+const handleOpenDetail = (row) => {
+  dataObj.detailObj = row;
+  detailDrawerApi.open();
+};
+const tabsData = ref([
+  { label: '全部' },
+  { label: '待核验' },
+  { label: '已确认' },
+  { label: '已使用' },
+  { label: '已取消' },
+  { label: '已过期' },
+]);
+const createLabel = (item) => {
+  let text = `(${dataObj.apilist.filter((v) => v.status === item.label).length})`;
+  if (item.label === '全部') {
+    text = `(${dataObj.apilist.length})`;
+  }
+  return item.label + text;
+};
 const handleClick = () => {
   gridApi.query();
 };
@@ -243,8 +285,88 @@ const handleFullShow = () => {
     <FormDrawer :title="getTitle">
       <Form />
     </FormDrawer>
-    <!-- <NewFormModel @success="handleRefresh" /> -->
-    <searchDrawer title="搜索栏设置" />
+    <DetailDrawer :title="`${dataObj.detailObj.name}关联表`">
+      <div class="detail-card">
+        <div class="detail-card-row">
+          <div class="detail-row-left">用户ID:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.user_id }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">车场ID:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.lot_id }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">车牌号:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.car_number }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">预约日期:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.reserve_date }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">开始时间:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.start_time }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">结束时间:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.end_time }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">预约状态:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.status }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">考核时间:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.verify_time }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">核验人:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.verify_by }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">取消时间:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.cancel_time }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">取消原因:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.cancel_reason }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">创建时间:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.create_time }}
+          </div>
+        </div>
+        <div class="detail-card-row">
+          <div class="detail-row-left">更新时间:</div>
+          <div class="detail-row-right">
+            {{ dataObj.detailObj.update_time }}
+          </div>
+        </div>
+      </div>
+    </DetailDrawer>
     <Drawer title="搜索">
       <QueryForm class="query-form" />
     </Drawer>
@@ -261,7 +383,7 @@ const handleFullShow = () => {
               <el-tab-pane
                 v-for="item in tabsData"
                 :key="item.label"
-                :label="item.label"
+                :label="createLabel(item)"
                 :name="item.label"
               />
             </el-tabs>
@@ -272,7 +394,7 @@ const handleFullShow = () => {
         <TableAction
           :actions="[
             {
-              label: textObj.addText,
+              label: '新增',
               type: 'primary',
               icon: ACTION_ICON.ADD,
               auth: ['system:role:create'],
@@ -316,9 +438,26 @@ const handleFullShow = () => {
           ></i>
         </button>
       </template>
+      <template #parkName="{ row }">
+        <el-text
+          @click="handleOpenDetail(row)"
+          class="common-align"
+          type="primary"
+        >
+          {{ row.name }}
+        </el-text>
+      </template>
       <template #actions="{ row }">
         <TableAction
           :actions="[
+            {
+              label: '详情',
+              type: 'primary',
+              link: true,
+              icon: ACTION_ICON.MORE,
+              auth: ['system:role:update'],
+              onClick: handleOpenDetail.bind(null, row),
+            },
             {
               label: '编辑',
               type: 'primary',
@@ -342,9 +481,18 @@ const handleFullShow = () => {
         />
       </template>
       <template #bottom>
-        <span class="bottom-title">
-          {{ textObj.total }}
-        </span>
+        <div class="common-total" @click="changeTotalShow">
+          <el-icon class="tabel-tab-icon" v-if="!dataObj.totalShow">
+            <ArrowDown />
+          </el-icon>
+          <el-icon class="tabel-tab-icon" v-if="dataObj.totalShow">
+            <ArrowUp />
+          </el-icon>
+          <span> 本页统计：停车场数量10;车位总数:326;车场车位117 </span>
+        </div>
+        <div class="common-total-bottom" v-if="dataObj.totalShow">
+          <span> 全部统计：{{ textObj.total }} </span>
+        </div>
       </template>
     </Grid>
   </div>
