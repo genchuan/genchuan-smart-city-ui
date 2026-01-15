bc570cbd23d32eedda0bc1819254e84b1cfc0764
LINAN <lince@genchuan.cn>
2026-01-15 11:28:49 +0800

    feat: 路由切换标签名更改

--- apps/web-ele/src/app.vue
+++ apps/web-ele/src/app.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
+
 import { useElementPlusDesignTokens } from '@vben/hooks';
 
 import { ElConfigProvider } from 'element-plus';
@@ -6,6 +9,21 @@ import { ElConfigProvider } from 'element-plus';
 import { elementLocale } from '#/locales';
 
 defineOptions({ name: 'App' });
+
+// 获取当前路由实例（响应式）
+const route = useRoute();
+
+// 监听路由完整变化（包括路径、参数、查询参数）
+watch(
+  // 监听的目标：route.fullPath（包含路径+参数+查询参数）
+  () => route.fullPath,
+  // 回调函数：newVal 新路由，oldVal 旧路由
+  () => {
+    document.title = '智慧城市一网统管AI平台';
+  },
+  // 可选配置：immediate 立即执行（组件挂载时触发一次）
+  { immediate: true },
+);
 
 useElementPlusDesignTokens();
 </script>
