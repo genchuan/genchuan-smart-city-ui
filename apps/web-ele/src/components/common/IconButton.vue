a62ffa0477b557bcd656f3cfd3cdb8abc12c234b
LINAN <lince@genchuan.cn>
2026-01-19 16:37:59 +0800

    组件图标封装

---
+++ apps/web-ele/src/components/common/IconButton.vue
@@ -0,0 +1,73 @@
+<script setup>
+// 定义组件 Props
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  content: {
+    type: String,
+    default: '新增',
+  },
+  placement: {
+    type: String,
+    default: 'top',
+  },
+  iconName: {
+    type: String,
+    default: 'Plus',
+  },
+  color: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: '',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+// 方式1：JS 形式声明自定义事件（无类型标注）
+const emit = defineEmits(['click']);
+
+// 手动触发自定义点击事件
+const handleManualClick = (obj) => {
+  // 触发自定义事件，可携带参数
+  emit('click', obj);
+};
+</script>
+<template>
+  <el-popover
+    :title="props.title"
+    :content="props.content"
+    :placement="props.placement"
+  >
+    <template #reference>
+      <el-button
+        :type="props.type"
+        :disabled="props.disabled"
+        class="genchuan-common-button"
+        @click="handleManualClick"
+      >
+        <el-icon class="common-icon">
+          <component :is="props.iconName" :color="props.color" />
+        </el-icon>
+      </el-button>
+    </template>
+  </el-popover>
+</template>
+<style lang="scss">
+.genchuan-common-button {
+  padding: 5px;
+  border-radius: 50%;
+}
+.common-icon {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  padding: 2px;
+}
+</style>
