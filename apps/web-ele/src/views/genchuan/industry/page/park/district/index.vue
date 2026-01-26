83748857e3a229e7afcc538ed8a9139fd64b35af
LINAN <lince@genchuan.cn>
2026-01-26 15:35:45 +0800

    样式自适应修改

---
+++ apps/web-ele/src/views/genchuan/industry/page/park/district/index.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref } from 'vue';
+
+import Parkchart from './parkchart.vue';
+import Table from './table/index.vue';
+
+import '#/components/page/index.scss';
+
+const changeArrowStatus = () => {
+  secondShow.value = !secondShow.value;
+  tabArray.value.forEach((v) => {
+    v.secondShow = secondShow.value;
+  });
+};
+const arrowChange = () => {
+  tabArray.value.forEach((v) => {
+    v.arrowShow = !v.arrowShow;
+  });
+};
+const tabArray = ref([
+  {
+    label: '片区基础管理',
+    components: Table,
+    showSecondary: true,
+    secondShow: false,
+    arrowShow: true,
+    arrowState: false,
+  },
+  {
+    label: '片区关联维护',
+    components: Table,
+    showSecondary: true,
+    secondShow: false,
+    arrowShow: true,
+    arrowState: false,
+  },
+]);
+const activeName = ref('片区基础管理');
+const secondShow = ref(false);
+</script>
+<template>
+  <div class="common-index">
+    <Parkchart v-if="tabArray[0].arrowShow" />
+    <div class="icon-change">
+      <el-icon
+        class="tabel-tab-icon"
+        v-if="secondShow"
+        @click="changeArrowStatus"
+      >
+        <ArrowDown />
+      </el-icon>
+      <el-icon
+        class="tabel-tab-icon"
+        v-if="!secondShow"
+        @click="changeArrowStatus"
+      >
+        <ArrowUp />
+      </el-icon>
+    </div>
+    <el-tabs
+      v-model="activeName"
+      class="common-tabs"
+      type="card"
+      @tab-change="tabChange"
+    >
+      <el-tab-pane
+        v-for="item in tabArray"
+        :key="item.label"
+        :name="item.label"
+      >
+        <template #label>
+          <div class="table-first">
+            <span>{{ item.label }}</span>
+          </div>
+        </template>
+        <component
+          :is="item.components"
+          :second-show="item.secondShow"
+          :key="item.label"
+          :arrow-show="item.arrowShow"
+          @arrow-change="arrowChange"
+        />
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
