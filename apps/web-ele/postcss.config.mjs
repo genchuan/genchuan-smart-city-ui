d36265999900233c09120a5ac19b937bc7f9b8d2
guoyuanhong <guoyuanhong@genchuankeji.cn>
2026-04-13 09:41:21 +0000

    Edit postcss.config.mjs

--- apps/web-ele/postcss.config.mjs
+++ apps/web-ele/postcss.config.mjs
@@ -1,10 +1,1 @@
-// export { default } from '@vben/tailwind-config/postcss';
-import baseConfig from '@vben/tailwind-config/postcss';
-
-export default {
-  ...baseConfig,
-  tailwindcss: {
-    // 👇 关键：关闭对第三方CSS的@layer严格检查
-    respectLayers: false,
-  },
-};
+export { default } from '@vben/tailwind-config/postcss';
