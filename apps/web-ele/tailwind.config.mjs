5194e991758d91da7aec439f7a77d9701bcf5163
guoyuanhong <guoyuanhong@genchuankeji.cn>
2026-04-13 09:34:07 +0000

    Edit tailwind.config.mjs

--- apps/web-ele/tailwind.config.mjs
+++ apps/web-ele/tailwind.config.mjs
@@ -1,12 +1,1 @@
-import baseConfig from '@vben/tailwind-config';
-
-export default {
-  ...baseConfig,
-  future: {
-    respectDefaultLayers: false,
-  },
-  corePlugins: {
-    preflight: false,
-  },
-  content: [],
-};
+export { default } from '@vben/tailwind-config';
