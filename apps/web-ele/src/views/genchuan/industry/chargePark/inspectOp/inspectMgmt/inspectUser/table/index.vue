ef707575f6e834d6eb648e9ed1b5b825e2be0c42
whc <952987912@qq.com>
2026-04-22 16:35:21 +0800

    fix: 修改巡检人员事件处理

--- apps/web-ele/src/views/genchuan/industry/chargePark/inspectOp/inspectMgmt/inspectUser/table/index.vue
+++ apps/web-ele/src/views/genchuan/industry/chargePark/inspectOp/inspectMgmt/inspectUser/table/index.vue
@@ -250,13 +250,13 @@ function handleEdit(row) {
   formDrawerRef.value?.open(row);
 }
 
-// function handleEnable(row) {
-//   statusConfirmDialogRef.value?.open('enable', row);
-// }
+function handleEnable(row) {
+  statusConfirmDialogRef.value?.open('enable', row);
+}
 
-// function handleDisable(row) {
-//   statusConfirmDialogRef.value?.open('disable', row);
-// }
+function handleDisable(row) {
+  statusConfirmDialogRef.value?.open('disable', row);
+}
 
 function handleDeviceClick(row) {
   dataObj.deviceDetailObj = getDeviceDetail(row);
@@ -484,7 +484,7 @@ watch(
             icon-name="Edit"
             @click="handleEdit(row)"
           />
-          <!-- <IconButton
+          <IconButton
             v-if="isUserStatusLabel(row.status, '正常')"
             content="禁用"
             icon-name="VideoPause"
@@ -495,7 +495,7 @@ watch(
             content="启用"
             icon-name="CircleCheckFilled"
             @click="handleEnable(row)"
-          /> -->
+          />
           <IconButton
             content="查看"
             icon-name="View"
