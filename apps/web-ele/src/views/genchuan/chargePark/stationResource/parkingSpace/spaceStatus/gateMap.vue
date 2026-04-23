6fa7eed4699c9349ebc4d661629dd97a7ff41307
WINDOW <m18563775462@163.com>
2026-04-23 23:18:46 +0800

    增加泊车模块管理

---
+++ apps/web-ele/src/views/genchuan/chargePark/stationResource/parkingSpace/spaceStatus/gateMap.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Map from '#/genchuan-components/Map/index.vue';
+
+defineProps({
+  data: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div class="station-map-panel">
+    <Map :data="data" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.station-map-panel {
+  min-height: 340px;
+  overflow: hidden;
+  background-color: hsl(var(--card));
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgb(0 0 0 / 8%);
+}
+</style>
