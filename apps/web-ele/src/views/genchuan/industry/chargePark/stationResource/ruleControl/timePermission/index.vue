362e3432d894214a4812820820e8f8c9edc0d603
WINDOW <m18563775462@163.com>
2026-04-27 17:14:56 +0800

    refactor: 调整充电场站目录结构

--- apps/web-ele/src/views/genchuan/industry/chargePark/stationResource/ruleControl/timePermission/index.vue
+++ apps/web-ele/src/views/genchuan/industry/chargePark/stationResource/ruleControl/timePermission/index.vue
@@ -185,14 +185,25 @@ function getPlaceholder(field) {
 function createSchema(fields, isSearch = false) {
   return fields.map((field) => {
     let component;
-    if (field.type === 'select') {
-      component = 'Select';
-    } else if (field.type === 'number') {
-      component = 'InputNumber';
-    } else if (field.type === 'date') {
-      component = 'DatePicker';
-    } else {
-      component = 'Input';
+    switch (field.type) {
+      case 'date': {
+        component = 'DatePicker';
+
+        break;
+      }
+      case 'number': {
+        component = 'InputNumber';
+
+        break;
+      }
+      case 'select': {
+        component = 'Select';
+
+        break;
+      }
+      default: {
+        component = 'Input';
+      }
     }
     const componentProps = {
       placeholder: getPlaceholder(field),
