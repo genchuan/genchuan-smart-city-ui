040529d7ad1152547ba382a821dc89503f622129
DaPeGood <944782441@qq.com>
2026-05-09 17:09:17 +0800

    互联互通、订单列表bug修复

--- apps/web-ele/src/api/genchuan/industry/energyCharging/carCharging/settlement/sharingratio/index.js
+++ apps/web-ele/src/api/genchuan/industry/energyCharging/carCharging/settlement/sharingratio/index.js
@@ -39,7 +39,6 @@ export function getSharingRatio(params) {
   });
 }
 
-
 /** 复制 */
 export function copySharingRatio(data) {
   return requestClient.post('/vehiclecharging/sharing-ratio/copy', data);
