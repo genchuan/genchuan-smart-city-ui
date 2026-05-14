6594812c491d4e268c57fadfba95ad87850a2d4d
Linan <243436318@qq.com>
2026-05-14 16:05:22 +0800

    618 【共享充电订单】列表备用字段可隐藏

--- apps/web-ele/src/views/genchuan/industry/chargePark/orderTrade/orderMgmt/shareChargeOrder/table/data.js
+++ apps/web-ele/src/views/genchuan/industry/chargePark/orderTrade/orderMgmt/shareChargeOrder/table/data.js
@@ -323,25 +323,7 @@ export function useGridColumns() {
       minWidth: 220,
       sortable: true,
       customRender: ({ text }) => text || '-',
-    },
-    {
-      field: 'operatorId',
-      title: '操作人ID',
-      minWidth: 120,
-      sortable: true,
-    },
-    {
-      field: 'reserve1',
-      title: '备用字段1',
-      minWidth: 120,
-      sortable: true,
-    },
-    {
-      field: 'reserve2',
-      title: '备用字段2',
-      minWidth: 120,
-      sortable: true,
-    },
+    }, 
     {
       field: 'creator',
       title: '创建者',
